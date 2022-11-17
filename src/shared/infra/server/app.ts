42b24efafc61daa7cb57cefa450634c75d65309d
Luiz Gustavo <gusstavopdm@gmail.com>
2022-11-17 09:33:58 -0300

    feat(app): adicionando morganMiddleware

--- src/shared/infra/server/app.ts
+++ src/shared/infra/server/app.ts
@@ -5,11 +5,15 @@ import helmet from 'helmet';
 import 'dotenv/config';
 import 'reflect-metadata';
 
+import { morganMiddleware } from './middlewares/morgan';
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morganMiddleware);
 
 export { app };
