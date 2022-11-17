e2d10cf4a7411a992f66a59fa4e1d7c8b1755b0a
Luiz Gustavo <gusstavopdm@gmail.com>
2022-11-17 09:34:23 -0300

    feat(server): adicionando Logger.info

--- src/shared/infra/server/server.ts
+++ src/shared/infra/server/server.ts
@@ -2,10 +2,11 @@ import http from 'http';
 
 import { apiConfig } from '@config/index';
 
+import { Logger } from '../logging/logger';
 import { app } from './app';
 
 const server = http.createServer(app);
 
 server.listen(apiConfig.port, () => {
-  console.log(`Server listening on port ${apiConfig.port}`);
+  Logger.info(`Server listening on port ${apiConfig.port}`);
 });
