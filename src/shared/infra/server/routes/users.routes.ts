630e314a36c44125f3c931139412105e8375376d
Luiz Gustavo <gusstavopdm@gmail.com>
2022-11-17 22:29:41 -0300

    feat(usersRoutes): adicionando validatePayload, createUser

--- src/shared/infra/server/routes/users.routes.ts
+++ src/shared/infra/server/routes/users.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController';
+import { CreateUserSchema } from '@modules/users/useCases/createUser/CreateUserSchema';
+
+import { validatePayload } from '../middlewares/validatePayload';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 
-usersRoutes.post('/registration', createUserController.handle);
+usersRoutes.post(
+  '/registration',
+  validatePayload(CreateUserSchema),
+  createUserController.handle,
+);
 
 export { usersRoutes };
